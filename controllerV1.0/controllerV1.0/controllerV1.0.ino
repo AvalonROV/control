1b9b1350af4da2f3043ea6316b636a327c9aef91
Dom RG <annoyingmouse.drg@gmail.com>
2019-03-01 13:10:35 +0000

    PID loop
    Adapted pid loop, needs testing and tuning

--- controllerV1.0/controllerV1.0/controllerV1.0.ino
+++ controllerV1.0/controllerV1.0/controllerV1.0.ino
@@ -17,52 +17,63 @@
  * Tune and store in EEPROM
  */
 
-int thrusterPin[6] = {0,1,2,3,4,5};
-int pressurePin = 6;
+int thrusterPin[2] = {9,10};
+
+Servo up1,up2;
+MS5803 sensor(0x76);
 
 unsigned int currLoop = 0;
 unsigned int prevLoop = 0;
 int loopTime = 0;
-float thrusterSpeeds[6] = {}; //uf,ub,fl,fr,bl,br
-float k[3] = {0.5,0,0};
-float desiredDepth = 0;
+float thrusterSpeeds[2] = {1500,1500}; //uf,ub,fl,fr,bl,br
+double pressure;
+float desiredPressure = 1030;
+double pressureErrorMargin = 1.0;
 float error[3] = {0,0,0}; //p,i,d
+float k[3] = {0.05,0,0.1}; //p,i,d
 
 
 
 void setup() {
-  for(int x = 0; x < 6; x ++){
-    pinMode(thrusterPin[x],OUTPUT);
-  }
-  pinMode(pressurePin,INPUT);
+  up1.attach(thrusterPin[0]);
+  up1.writeMicroseconds(1500); // send "stop" signal to ESC.
+  up2.attach(thrusterPin[1]);
+  up2.writeMicroseconds(1500); // send "stop" signal to ESC.
+  delay(1000);
+  sensor.reset();
+  sensor.begin();
   Serial.begin(115200);
 }
 
-void loop(float* persistentVariables) { //persistentVariables[]{prevLoop,
+void loop() { //persistentVariables[]{prevLoop,
   currLoop = millis();
   //Poll sensors (pressure and controller)
-  getDepth();
+  fetch_depth();
   
   loopTime = currLoop - prevLoop;
   //calculations
   calculateError();
-  
+  int dT = currLoop - prevLoop;
   //PID
-  thrusterSpeeds[0] += k[0]*error[0] + dT*k[1]*error[1] + iT*k[2]*error[2];
-  thrusterSpeeds[1] += k[0]*error[0] + dT*k[1]*error[1] + iT*k[2]*error[2];
-  
+  thrusterSpeeds[0] += k[0]*error[0] + k[1]*error[1]*dT + k[2]*(error[2]/dT);
+  thrusterSpeeds[1] += k[0]*error[0] + k[1]*error[1]*dT + k[2]*(error[2]/dT);
+  Serial.println(); Serial.print(pressure); Serial.print("   "); Serial.print(thrusterSpeeds[1]);
   updateThrusters();
   prevLoop = currLoop;
 }
 
 float fetch_depth(){
-  //
+  pressure = sensor.getPressure(ADC_4096);
 }
 
 void calculateError(){
-  
+  float prevError = error[0];
+  error[0] = desiredPressure - pressure;
+  error[1] += error[0];
+  error[2] = error[0] - prevError;
 }
 
 void updateThrusters(){
-  //update all thruster speeds
+  up1.writeMicroseconds(thrusterSpeeds[0]);
+  up2.writeMicroseconds(thrusterSpeeds[1]);
 }
