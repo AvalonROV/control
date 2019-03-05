7b7ff117f9b4d8e84e841d874b6bab90aff28442
Dom RG <annoyingmouse.drg@gmail.com>
2019-03-05 14:11:58 +0000

    Mk1
    PID and tuning refined and first version complete

--- controllerV1.0/controllerV1.0/controllerV1.0.ino
+++ controllerV1.0/controllerV1.0/controllerV1.0.ino
@@ -22,15 +22,15 @@ int thrusterPin[2] = {9,10};
 Servo up1,up2;
 MS5803 sensor(0x76);
 
-unsigned int currLoop = 0;
-unsigned int prevLoop = 0;
-int loopTime = 0;
+unsigned long currLoop = 0;
+unsigned long prevLoop = 0;
 float thrusterSpeeds[2] = {1500,1500}; //uf,ub,fl,fr,bl,br
 double pressure;
-float desiredPressure = 1030;
+float desiredPressure = 1010;
 double pressureErrorMargin = 1.0;
 float error[3] = {0,0,0}; //p,i,d
-float k[3] = {0.05,0,0.1}; //p,i,d
+float k[3] = {12,0.01,0.2}; //p,i,d
+float dT = 0;
 
 
 
@@ -50,16 +50,32 @@ void loop() { //persistentVariables[]{prevLoop,
   //Poll sensors (pressure and controller)
   fetch_depth();
   
-  loopTime = currLoop - prevLoop;
   //calculations
   calculateError();
-  int dT = currLoop - prevLoop;
+  dT = currLoop - prevLoop;
   //PID
-  thrusterSpeeds[0] += k[0]*error[0] + k[1]*error[1]*dT + k[2]*(error[2]/dT);
-  thrusterSpeeds[1] += k[0]*error[0] + k[1]*error[1]*dT + k[2]*(error[2]/dT);
-  Serial.println(); Serial.print(pressure); Serial.print("   "); Serial.print(thrusterSpeeds[1]);
+  thrusterSpeeds[0] = 1500 + k[0]*error[0] + k[1]*error[1]*dT + k[2]*(error[2]/dT);
+  thrusterSpeeds[1] = 1500 + k[0]*error[0] + k[1]*error[1]*dT + k[2]*(error[2]/dT);
+  for(int x = 0; x < 2; x++){
+    thrusterSpeeds[x] = limit(thrusterSpeeds[x],1900,1100);
+  }
+  Serial.println(); Serial.print(map(pressure,1030,1000,0,30)); //Serial.print("\t"); Serial.print(thrusterSpeeds[0]);
   updateThrusters();
   prevLoop = currLoop;
+  //Serial.print("\t");Serial.print(dT); //current sample rate = 25ms
+  //delay(25);
+}
+
+float limit(float toLimit,int upLim,int lowLim){
+  if(toLimit>upLim){
+    return upLim;
+  }
+  else if(toLimit<lowLim){
+    return lowLim;
+  }
+  else{
+    return toLimit;
+  }
 }
 
 float fetch_depth(){
@@ -71,9 +87,10 @@ void calculateError(){
   error[0] = desiredPressure - pressure;
   error[1] += error[0];
   error[2] = error[0] - prevError;
+  //Serial.print("\t");Serial.print(error[2]);
 }
 
 void updateThrusters(){
-  up1.writeMicroseconds(thrusterSpeeds[0]);
-  up2.writeMicroseconds(thrusterSpeeds[1]);
+  up1.writeMicroseconds(int(thrusterSpeeds[0]));
+  up2.writeMicroseconds(int(thrusterSpeeds[1]));
 }
